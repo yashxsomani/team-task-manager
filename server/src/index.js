a93b152ca8a4eff30a6222a998b04d8e0f2e47c0
yashxsomani <yashsomani365@gmail.com>
2026-05-03 20:25:38 +0530

    fix cors

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: function(origin, callback) {
+    callback(null, true);
+  },
   credentials: true
 }));
 app.use(express.json());
